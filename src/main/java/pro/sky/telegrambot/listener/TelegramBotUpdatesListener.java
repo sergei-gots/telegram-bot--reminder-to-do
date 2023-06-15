109409feb25717f0543c53073613e67b3ff42431
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-06-16 00:53:56 +0300

    liquibase draft scheme added

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.function.Consumer;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -28,7 +30,14 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            Long chatId= update.message().chat().id();
+            logger.info("Processing update: text={}, chat-id={}",
+                    update.message().text(),
+                    chatId
+            );
             // Process your updates here
+            telegramBot.execute(new SendMessage(chatId,
+                    "Hello, " + update.message().authorSignature() + "!"));
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
